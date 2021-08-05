21b2c4bae261e8fa23d5808059e359bb3cda8041
Pawel <zmarlakpawel@gmail.com>
2021-08-05 15:26:50 +0000

    Getrecords pokazuje ostatnio dodaną pozycje w divie, receiver przyjmujestring z extension

--- app/src/Controller/GetrecordsController.php
+++ app/src/Controller/GetrecordsController.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Cake\Datasource\ConnectionManager;
 use Cake\Event\EventInterface;
 use Cake\Core\Configure;
+use Cake\Log\Log;
+// use Cake\Log\Engine\BaseLog;
+// use Psr\Log\LoggerInterface;
 
 /**
  * Getrecords Controller
@@ -103,6 +107,7 @@ class GetrecordsController extends AppController
             $this->Flash->error(__('The getrecord could not be deleted. Please, try again.'));
         }
 
+        
         return $this->redirect(['action' => 'index']);
     }
 
@@ -115,20 +120,52 @@ class GetrecordsController extends AppController
     //api shows one record values to the extension that inputs them
     public function api(){
         //fetch records to check from the database (from getrecords)
-        $getrecord = $this->Getrecords->get($id, [
-            'contain' => [],
-        ]);
+        // $getrecord = $this->Getrecords->get($id, [
+        //     'contain' => [],
+        // ]);
+        // $connection = ConnectionManager::get('default');
+        // $results = $connection->execute('SELECT * FROM getrecords')
+        // ->fetchAll('assoc');
+        // echo $results;
 
-        $this->set(compact('getrecord'));
+        // $query = $this->Getrecords->execute('SELECT * FROM getrecords LIMIT 1');
+        // $this->layout = false;
+        $this->viewBuilder()->enableAutoLayout(false);
+        $onerecord = $this->Getrecords->find()->where(['id' => 1])->all(); //get latest(id DESC) limit 1
+        //najwyższe id jest najnowsze
+        $onerecord = $this->Getrecords
+            ->find()
+            ->order(['id' => 'DESC'])
+            ->first();
+
+        $encoded = json_encode($onerecord);
+        $env = $this->request->getServerParams();
+
+        $getrecords = $this->paginate($this->Getrecords);
+        $this->set(compact('getrecords', 'onerecord', 'encoded', 'env'));
+        Log::debug($encoded);
+        // $this->set(compact('getrecord'));
     }
 
     public function receiver(){
+        $data_str = file_get_contents('php://input');
+        $data = json_decode($data_str, true);
+        // Log::debug('cos');
+        Log::debug('json','sgsdgsdg');
+        Log::debug('json',$data_str);
+        debug($data);
+        echo $data;
         debug($_POST);
         $getrecords = $this->paginate($this->Getrecords);
         $this->set(compact('getrecords'));
         //receive a JSON response from the extension
-        echo $this->request->getHeader('x-cos-x'); //array to string conversion warning
+        // echo $this->request->getHeader('x-cos-x'); //array to string conversion warning
+        echo $this->request->getHeader('exampleHeaderName'); //array to string conversion warning
+
         //get request, perform operation, return the response
         $this->request->getData('');
+
+       // <div data-json="{'nr':'asdas',}" ><button>find dis </button></div>
+      //  <div data-json="{'nr':'asdas',}" ><button>find dis </button></div>
     }
 }
